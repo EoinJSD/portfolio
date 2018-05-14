dd8f3edbd2b1b3d3f9e5bbcb2d983b7de2ec8834
Eoin J S Dolan <eoin_dolan@thbs.com>
2018-05-14 15:55:32 +0100

    edit index

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <header><?php include_once('pages/inc/header.inc.php') ?></header>
     <section class="LandingZone"><?php include_once('pages/Bio.php') ?></section>
 
-  	<section class="ShowOff"><?php include_once('pages/ShowOff.php') ?></section>
+    <section class="Skills"><?php include_once('pages/Skills.php') ?></section>
+
+    <section class="ShowOff"><?php include_once('pages/ShowOff.php') ?></section>
 
     <section class="blogSection" id="index"><div class="blogContainer" id="scroll"><?php include_once('pages/BlogSection.php') ?></div></section>
-
-    <section class="Skills"><?php include_once('pages/Skills.php') ?></section>
 
     <footer><?php include_once('pages/inc/footer.inc.php') ?></footer>
 </body>

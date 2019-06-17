c9600b4ec65684aaaaa3cd3003d0d413c656e3f8
EoinJSD <eoinjsd@hotmail.com>
2019-06-17 23:25:49 +0100

    fixed config issues and some css

---
+++ pages/inc/footer.inc.php
@@ -0,0 +1,19 @@
+<div class="footerMain">
+<!--
+    <div class="formContainer">
+
+        <form class="contactForm" action="contact.php" method="post">
+          <h2 class="contact">Contact</h2>
+          <input type="text" id="name" name="name" placeholder="Full Name *" maxlength="50" required />
+          <input type="email" id="email" name="email" placeholder="Email Address *" maxlength="50" required />
+          <textarea type="text" id="message" name="message" placeholder="Message *" maxlength="500"> </textarea>
+          <button type="submit">Send</button>
+        </form>
+    </div>
+    <div class="spacer"></div>
+-->
+    <div class="signature">
+        <p>Ongoing development by: @EoinJSD</p>
+    </div>
+
+</div>
---
+++ pages/inc/header.inc.php
@@ -0,0 +1,14 @@
+<?php $url = $_SERVER['REQUEST_URI'];?>
+<nav><!--Centered container for Logo and Links-->
+  <div class="nav-container">
+    <div class="logo-container">
+        <a href="./index.php" class="logo"><img src="./images/eoinjsd.png" /></a>
+    </div>
+    <div class="links-container">
+      <ul class="links">
+        <li><a class="active" id="experience" href="./experience.php">Experience</a></li>
+        <li><a id="portfolio" href="./portfolio.php">Portfolio</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
